953da53fb84d3d62e007c658b43562fffa033259
=Meltus645 <=meltusjuma5@gmail.com>
2024-02-07 16:31:10 +0300

    Modified the Fibbonacci Sequence Program
    Added spacing and comments to enhance code readerbility

--- fibonacci_sequence.py
+++ fibonacci_sequence.py
@@ -1,13 +1,19 @@
 # Question 2: Fibonacci Sequence
+
 # Write a program to generate the Fibonacci sequence up to 100
 
-sequence = []
-limit =100
-prev, curr =(0, 1)
+limit =100 # the first <limit> numbers
 
-for _ in range(limit):
-    if prev >=limit: break
-    sequence.append(prev)
-    prev, curr =(curr, prev + curr)
+sequence = [] # empty list to store the sewquence
 
-print(sequence)
+prev, curr =(0, 1) # set the initial values of the sequence
+
+for _ in range(limit +1): # loop through the first <limit> numbers
+
+    if prev >limit: break # exit loop if the previous value exceeds the <limit>
+
+    sequence.append(prev) # add values to the sequence
+
+    prev, curr =(curr, prev + curr) # update the values
+
+print(sequence) # display the sequence
